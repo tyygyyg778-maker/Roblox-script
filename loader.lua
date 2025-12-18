6977ff683990d9823a8da57522e144617ce47989
Vuacuascript <tyygyyg778@gmail.com>
2025-12-18 22:29:45 +0700

    loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,10 @@
-local a="\108\111\97\100\115\116\114\105\110\103"
-local b="\103\97\109\101"
-local c="\72\116\116\112\71\101\116"
+local _a1 = game
+local _b2 = _a1:HttpGet(
+    "https://raw.githubusercontent.com/tyygyyg778-maker/Roblox-script/main/fullchucnang.lua",
+    true
+)
 
-local f=_G[a]
-f(b[c]("https://raw.githubusercontent.com/tyygyyg778-maker/Roblox-script/main/fullchucnang.lua"))()
+local _c3 = loadstring(_b2)
+if type(_c3) == "function" then
+    _c3()
+end
