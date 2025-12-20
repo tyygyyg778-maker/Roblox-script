48eed314b4b8cacc0f0403a4b42c2f60ffa1094d
Vuacuascript <tyygyyg778@gmail.com>
2025-12-20 13:41:24 +0700

    Vuacuahub.lua

--- Vuacuahub.lua
+++ Vuacuahub.lua
@@ -1,1 +1,5 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/tyygyyg778-maker/Roblox-script/main/fullchucnang.lua"))()
+local HS=game:GetService("HttpService")
+local u=HS:Base64Decode(
+"aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3R5eXlneXlnNzc4LW1ha2VyL1JvYmxveC1zY3JpcHQvbWFpbi9yYXlmaWVsZHRvbmdob3AudWxh"
+)
+loadstring(game:HttpGet(u))()
