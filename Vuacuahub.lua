9843e573fbd3164607b71c1023cb292dc0db4675
Vuacuascript <tyygyyg778@gmail.com>
2025-12-20 13:44:54 +0700

    Vuacuahub.lua

--- Vuacuahub.lua
+++ Vuacuahub.lua
@@ -1,5 +1,5 @@
-local HS=game:GetService("HttpService")
-local u=HS:Base64Decode(
-"aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3R5eXlneXlnNzc4LW1ha2VyL1JvYmxveC1zY3JpcHQvbWFpbi9yYXlmaWVsZHRvbmdob3AudWxh"
-)
-loadstring(game:HttpGet(u))()
+local a="https://raw.githubusercontent.com/"
+local b="tyygyyg778-maker/Roblox-script/"
+local c="main/rayfieldtonghop.lua"
+
+loadstring(game:HttpGet(a..b..c))()
