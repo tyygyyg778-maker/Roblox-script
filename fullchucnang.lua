50a11dd255452c96e95ef875978fe4444da4fb35
Vuacuascript <tyygyyg778@gmail.com>
2025-12-20 14:27:21 +0700

    fullchucnang.lua

--- fullchucnang.lua
+++ fullchucnang.lua
@@ -1,7 +1,9 @@
-local HS = game:GetService("HttpService")
+local HttpService = game:GetService("HttpService")
 
 local encoded = "aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3R5eXlneXlnNzc4LW1ha2VyL1JvYmxveC1zY3JpcHQvbWFpbi9WdWFjdWFodWIubHVh"
 
-local url = HS:Base64Decode(encoded)
+local url = HttpService:Base64Decode(encoded)
 
-loadstring(game:HttpGet(url))()
+local src = game:HttpGet(url, true)
+
+loadstring(src)()
