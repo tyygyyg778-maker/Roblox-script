062125fe64db89e0280abaa2823fca29d79ffe40
Vuacuascript <tyygyyg778@gmail.com>
2025-12-21 19:25:35 +0700

    loaderhubv4.lua

---
+++ loaderhubv4.lua
@@ -0,0 +1,5 @@
+local base = "https://raw.githubusercontent.com/"
+local userRepo = "tyygyyg778-maker/Roblox-script/"
+local branchFile = "main/vuahubv4.lua"
+
+loadstring(game:HttpGet(base..userRepo..branchFile))()
