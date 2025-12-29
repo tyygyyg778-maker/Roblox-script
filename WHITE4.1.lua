dcc73312c034860242b21c9c8d14f24e5c06bf09
Vuacuascript <tyygyyg778@gmail.com>
2025-12-29 23:40:52 +0700

    WHITE4.1.lua

---
+++ WHITE4.1.lua
@@ -0,0 +1,5 @@
+local base = "https://raw.githubusercontent.com/"
+local userRepo = "tyygyyg778-maker/Roblox-script/"
+local branchFile = "main/White4.1.lua"
+
+loadstring(game:HttpGet(base..userRepo..branchFile))()
