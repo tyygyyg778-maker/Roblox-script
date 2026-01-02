147fec2157e9358e11f7206487ddb10ebeeea3f9
Vuacuascript <tyygyyg778@gmail.com>
2026-01-03 04:01:24 +0700

    White4.1.lua

--- White4.1.lua
+++ White4.1.lua
@@ -5,7 +5,7 @@ local Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
 local CORRECT_KEY = "menuhub"
 local GET_KEY_LINK = "https://link-hub.net/2510474/i7rDTr1F8AgS"
 local SCRIPT_LINK = "https://raw.githubusercontent.com/tyygyyg778-maker/Roblox-script/main/whitehub.lua"
-local SCARE_SCRIPT = "https://raw.githubusercontent.com/tyygyyg778-maker/Roblox-script/refs/heads/main/do%E1%BA%A1ma.lua"
+local SCARE_SCRIPT = "https://codeberg.org/white-maker/white/raw/branch/main/saikey.lua"
 
 -- 🪟 WINDOW
 local Window = Rayfield:CreateWindow({
