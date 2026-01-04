2fbbfdd7c2cabf5d82d8e8d6bcd3e124f44474e5
Vuacuascript <tyygyyg778@gmail.com>
2026-01-04 17:18:04 +0700

    white4.2real.lua

---
+++ white4.2real.lua
@@ -0,0 +1,7 @@
+-- Load thanh loading
+loadstring(game:HttpGet("https://pastefy.app/C8QxgaD0/raw?part=thanhload.lua"))()
+
+-- Đợi 8 giây trước khi load hub
+task.delay(8, function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/tyygyyg778-maker/Roblox-script/refs/heads/main/white4.2.lua"))()
+end)
