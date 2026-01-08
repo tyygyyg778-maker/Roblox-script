cf6002fe4b2b73e3af88e634a6ea42a98ad69e39
Vuacuascript <tyygyyg778@gmail.com>
2026-01-08 22:37:28 +0700

    whitehub5.0.lua

--- whitehub5.0.lua
+++ whitehub5.0.lua
@@ -3826,7 +3826,7 @@ CheckItemBPCRBPCR = function(v502) --[[ Line: 0 ]] --[[ Name:  ]]
         end;
     end;
 end;
-local v505 = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/main.lua"))():MakeWindow({
+local v505 = loadstring(game:HttpGet("https://sirius.menu/rayfield"))():MakeWindow({
     Title = "white hub 5.0", 
     SubTitle = "by white", 
     SaveFolder = "White | white  5.0.lua"
